347bd6212c48d61a5e606cd111e77fcc87cfb3f7
Nadezhda <nadezhdadieslast@gmail.com>
2024-03-21 21:58:30 +0400

    changed the variable name and finished main()

--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ void heapify(float arr[], int heapSize, int rootIndex) {
     }
 }
 
-void heapSort(float arr[], int size) {
-    for (int i = size/2 - 1; i >= 0; i--) {
-        heapify(arr, size, i);
+void heapSort(float arr[], int childIndex) {
+    for (int i = childIndex / 2; i >= 0; i--) {
+        heapify(arr, childIndex, i);
     }
-    for (int i = size - 1; i > 0; i--) {
+    for (int i = childIndex - 1; i > 0; i--) {
         std::swap(arr[0], arr[i]);
         heapify(arr, i, 0);
     }
@@ -36,8 +36,7 @@ void printArray(float arr[], int size) {
 int main() {
     float arr[] = {1.2, 2.3, 1.11, 3.4, 5.5, 5.4, 5.3, 5.1, 1.5, 1.25, 5.41, 5.31, 5.11, 1.51, 1.251};
     int size = sizeof(arr) / sizeof(arr[0]);
-    for (float element: arr) {
-        std::cin >> element;
-    }
 
+    heapSort(arr, size);
+    printArray(arr, 15);
 }
